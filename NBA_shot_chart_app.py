87388d65a32e8b0979c704c3066617482c420c40
Pablo <61460287+pabsalmeron@users.noreply.github.com>
2021-01-11 00:36:32 +0100

    Update NBA_shot_chart_app.py

--- NBA_shot_chart_app.py
+++ NBA_shot_chart_app.py
@@ -1,18 +1,115 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[28]:
-
-
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+#from shot_chart import *
+from matplotlib.patches import mpl
 from nba_api.stats.endpoints import shotchartdetail
 import json
 import requests
-import pandas as pd
-import matplotlib as mpl
-from matplotlib.patches import Circle, Rectangle, Arc
-import matplotlib.pyplot as plt
 
-# Teams
+
+
+st.title('NBA Player Stats Explorer')
+
+st.markdown("""
+This app performs simple webscraping of NBA player stats data!
+* **Python libraries:** base64, pandas, streamlit
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+""")
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
+# Web scraping of NBA player stats
+@st.cache
+def load_data(year):
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
+    raw = raw.fillna(0)
+    playerstats = raw.drop(['Rk'], axis=1)
+    return playerstats
+playerstats = load_data(selected_year)
+
+
+sorted_unique_player = sorted(playerstats.Player)
+selected_player = st.sidebar.multiselect('Player', sorted_unique_player)
+
+prev_year = str(int(selected_year) - 1)
+formated_year = prev_year + '-' + str(selected_year)
+
+#player_name = st.sidebar.text_input('Enter a player name if you want')
+
+# Sidebar - Team selection
+sorted_unique_team = sorted(playerstats.Tm.unique())
+selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+
+# Sidebar - Position selection
+unique_pos = ['C','PF','SF','PG','SG']
+selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+
+# Filtering data
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
+
+#def check_data(df, player, selected_pos=selected_pos, selected_team=selected_team):
+#    if player not in df.Player:
+#        no_player_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
+#        return no_player_team
+#    else:
+#        player_team =  playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos)) & (playerstats['Player'] == player_name)]
+#        return player_team
+
+#df_selected_team = check_data(playerstats, player_name)
+
+df_selected_player = playerstats[playerstats.Player.isin(selected_player)]
+
+st.header('Player Stats That You Are Looking For! ')
+st.markdown("""
+*Just Write in the Search Box!*
+""")
+st.dataframe(df_selected_player)
+
+st.header('Display Players Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
+
+
+
+# Download NBA player stats data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+
+
+st.markdown("""
+*Display an Amazing Heatmap!*
+""")
+ Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    df_selected_team.to_csv('output.csv',index=False)
+    df = pd.read_csv('output.csv')
+
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=(7, 5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+    st.pyplot(f) 
+
+st.markdown("""
+*Display an Amazing Shot Chart!*
+""")
+
 teams = json.loads(requests.get('https://raw.githubusercontent.com/bttmly/nba/master/data/teams.json').text)
 
 def get_team_id(team_name):
@@ -25,10 +122,9 @@ def get_team_id(team_name):
 players = json.loads(requests.get('https://raw.githubusercontent.com/bttmly/nba/master/data/players.json').text)
 
 #SOLO MODIFICAR ESTOS PARÁMETROS
-name = 'Stephen'
-lastname = 'Curry'
-selected_team = 'Golden State Warriors'
-selected_year = '2018-19'
+name = [i.split(' ')[0] for i in selected_player]
+lastname = [i.split(' ')[1] for i in selected_player]
+
 def get_player_id(first, last):
     for player in players:
         if player['firstName'] == first and player['lastName'] == last:
@@ -39,17 +135,17 @@ shot_json = shotchartdetail.ShotChartDetail(
             team_id = get_team_id(f'{selected_team}'),
             player_id = get_player_id(f'{name}', f'{lastname}'),
             context_measure_simple = 'PTS',
-            season_nullable = f'{selected_year}',
+            season_nullable = f'{formated_year}',
             season_type_all_star = 'Regular Season')
 
 shot_data = json.loads(shot_json.get_json())
 
 relevant_data = shot_data['resultSets'][0]
 headers = relevant_data['headers']
-rows = relevant_data['rowSet']
+shots = relevant_data['rowSet']
 
 # Create pandas DataFrame
-player_data = pd.DataFrame(rows)
+player_data = pd.DataFrame(shots)
 player_data.columns = headers
 
 player_data.columns
@@ -98,14 +194,7 @@ ax = create_court(ax, 'black')
 ax.hexbin(player_data['LOC_X'], player_data['LOC_Y'] + 60, gridsize=(30, 30), extent=(-300, 300, 0, 940), bins='log', cmap='Blues')
 
 # Annotate player name and season
-fig.text(0, 1.05, f'{name} {lastname} \n{selected_year} Regular Season', transform=ax.transAxes, ha='left', va='baseline')
+fig.text(0, 1.05, f'{name} {lastname} \n{formated_year} Regular Season', transform=ax.transAxes, ha='left', va='baseline')
 ax.text(0, -0.075, 'Author: Pablo Salmerón', transform=ax.transAxes, ha='left')
 
-plt.show()
-
-
-# In[ ]:
-
-
-
-
+st.pyplot(fig)
